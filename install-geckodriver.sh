4c3b53fd2ad182bb796626db687594492129eaf6
Jan Reusch <jan@jreusch.de>
2021-02-18 11:32:56 +0100

    geckodriver now also ships signatures, yay. lets ignore them

--- install-geckodriver.sh
+++ install-geckodriver.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 INSTALL_DIR="/usr/local/bin"
 
-curl -s -L $(curl -sL https://api.github.com/repos/mozilla/geckodriver/releases/latest | jq -r '.assets[].browser_download_url' | grep linux64) | tar -xz
+curl -s -L $(curl -sL https://api.github.com/repos/mozilla/geckodriver/releases/latest | jq -r '.assets[].browser_download_url' | grep linux64 | grep -v asc) | tar -xz
 chmod +x geckodriver
 mv geckodriver "$INSTALL_DIR"
 echo "installed geckodriver binary in $INSTALL_DIR"
